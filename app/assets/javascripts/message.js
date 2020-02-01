3995905ad4eff41b092a832ff21e96de92a4dc76
toshihitoendou <59546465+toshihitoendou@users.noreply.github.com>
2020-02-01 16:00:45 +0900

    自動更新機能実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -40,9 +40,12 @@ $(function(){
    };
  }
 $('#new_message').on('submit', function(e){
+ // get words from send-box   send#
  e.preventDefault();
  var formData = new FormData(this);
  var url = $(this).attr('action')
+
+ //  how to send to routing  
  $.ajax({
    url: url,
    type: "POST",
@@ -51,6 +54,9 @@ $('#new_message').on('submit', function(e){
    processData: false,
    contentType: false
  })
+
+ // go to controller  
+
   .done(function(data){
     var html = buildHTML(data);
     $('.messages').append(html);      
@@ -61,6 +67,42 @@ $('#new_message').on('submit', function(e){
   })
   .always(function(){
     $(".submit-btn").removeAttr("disabled");
+    
   });
 })
+var reloadMessages = function() {
+  //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+  last_message_id = $('.message:last').data("message-id");
+  $.ajax({
+    //ルーティングで設定した通りのURLを指定
+    url: "api/messages",
+    //ルーティングで設定した通りhttpメソッドをgetに指定
+    type: 'get',
+    dataType: 'json',
+    //dataオプションでリクエストに値を含める
+    data: {id: last_message_id}
+  })
+  .done(function(messages) {
+    if (messages.length !== 0) {
+      //追加するHTMLの入れ物を作る
+      var insertHTML = '';
+      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      //メッセージが入ったHTMLに、入れ物ごと追加
+      $('.messages').append(insertHTML);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+    }
+  })
+  .fail(function() {
+    console.log('error');
+  });
+};
+if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  setInterval(reloadMessages, 7000);
+}
 });
+
+
+
